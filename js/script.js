6ce296a2ca1e5004f95703ddcba9934cd6eef3de
zrodrigues <83433096+zrodrigues@users.noreply.github.com>
2021-04-29 20:33:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    // setTimeout(function () {
+    //     $('#splash-red').slideUp();
+    // }, 500);
+    // setTimeout(function () {
+    //     $('#splash-black').slideDown();
+    // }, 510);
+    // $('.tr-offcanvas-close').click(function(){
+    //     UIkit.offcanvas('#offcanvas-flip').hide();
+    //     return false;
+    // })
+})
